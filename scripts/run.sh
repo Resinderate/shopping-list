cf6da2acac9c15132d1e15597433a1499a72771d
Ronan Murphy <ronan.murphy@originmarkets.com>
2019-01-20 22:48:13 +0000

    Typo in requirements.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "`dirname $0`/.."
 git pull
-pip install -r requirments.txt
+pip install -r requirements.txt
 
 export FLASK_APP=/home/pi/dev/shopping-list/shopping-list.py
 flask run --host=0.0.0.0 --port=5001
